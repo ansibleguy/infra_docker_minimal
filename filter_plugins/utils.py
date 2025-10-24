22b3dc723d0536f94528d242e0646d3f12327563
Rath Pascal <rath@oxl.at>
2025-10-25 01:20:01 +0200

    add option to configure forward-proxy for docker-daemon

---
+++ filter_plugins/utils.py
@@ -0,0 +1,16 @@
+class FilterModule(object):
+    def filters(self):
+        return {
+            "ensure_list": self.ensure_list,
+        }
+
+    @staticmethod
+    def ensure_list(data: (str, list)) -> list:
+        # if user supplied a string instead of a list => convert it to match our expectations
+        if data is None:
+            return []
+
+        if isinstance(data, list):
+            return data
+
+        return [data]
